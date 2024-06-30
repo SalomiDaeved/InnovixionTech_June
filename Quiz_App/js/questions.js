d797b555cba5d3114b55e29c28efa15a0c560cfe
SalomiDaeved <salomidaeved@gmail.com>
2024-06-30 22:15:15 +0530

    Add files via upload

---
+++ Quiz_App/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [{
+        numb: 1,
+        question: " What does CPU stand for in computing?",
+        answer: " Central Processing Unit",
+        options: [
+            "Computer Personal Unit",
+            "Central Peripheral Unit",
+            " Central Processing Unit",
+            "Computer Processing Unit"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following is a primary function of a firewall in a computer network?",
+        answer: "Blocking unauthorized access",
+        options: [
+            "Encrypting data",
+            "Monitoring network traffic",
+            "Blocking unauthorized access",
+            "Compressing files"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which protocol is used to securely transfer files over the internet?",
+        answer: "SFTP",
+        options: [
+            "FTP",
+            "HTTP",
+            "SSH",
+            "SFTP"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is the main purpose of an IP address in networking?",
+        answer: "To identify a network device",
+        options: [
+            " To store data",
+            "To identify a network device",
+            " To encrypt data",
+            "To monitor network traffic"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following best describes cloud computing?",
+        answer: "Delivery of computing services over the internet",
+        options: [
+            "A local storage solution",
+            "A system of interconnected computers",
+            "Delivery of computing services over the internet",
+            "A type of computer hardware"
+        ]
+    },
+
+];
